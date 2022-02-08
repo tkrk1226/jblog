fa2c65b2568dbd23abe65c1590a2826a037428f1
UijinJung <tkrk1226@ajou.ac.kr>
2022-02-08 16:18:40 +0900

    jblog03 # MainController search [220208-1618]

--- jblog03/src/main/java/com/poscoict/jblog/controller/MainController.java
+++ jblog03/src/main/java/com/poscoict/jblog/controller/MainController.java
@@ -2,12 +2,18 @@ package com.poscoict.jblog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-	@RequestMapping({"", "/main"})
+	@RequestMapping(value={"", "/main"}, method=RequestMethod.GET)
 	public String main() {
-		
 		return "main/index";
+	}
+	
+	@RequestMapping(value={"/main"}, method=RequestMethod.POST)
+	public String main(@RequestParam(value="keyword", required=false) String keyword) {
+		return "redirect:/" + keyword;
 	}
 }
--- jblog03/src/main/webapp/WEB-INF/views/main/index.jsp
+++ jblog03/src/main/webapp/WEB-INF/views/main/index.jsp
@@ -13,15 +13,10 @@
 	<div class="center-content">
 		<h1 class="logo">JBlog</h1>
 		<c:import url="/WEB-INF/views/includes/menu.jsp"/>	
-		<form class="search-form">
+		<form class="search-form" method="post" action="${pageContext.request.contextPath}/main">
 			<fieldset>
 				<input type="text" name="keyword" />
 				<input type="submit" value="검색" />
-			</fieldset>
-			<fieldset>
-				<input type="radio" name="which" value="blog-title"> <label>블로그 제목</label>
-				<input type="radio" name="which" value="tag"> <label>태그</label>
-				<input type="radio" name="which" value="blog-user"> <label>블로거</label>
 			</fieldset>
 		</form>
 	</div>
