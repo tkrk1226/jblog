3ddb248b5ce16fa83cf426bdd255a945005eb42f
UijinJung <tkrk1226@ajou.ac.kr>
2022-02-10 20:54:07 +0900

    jblog05 # exception [2202102054]

--- jblog05/src/main/java/com/poscoict/jblog/exception/GlobalExceptionHandler.java
+++ jblog05/src/main/java/com/poscoict/jblog/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 // AOP
 @ControllerAdvice
@@ -22,6 +23,12 @@ public class GlobalExceptionHandler {
 		// 혹은 서비스 론칭 전에 개발용으로 할때는 
 		// Error 내용을 뿌리는 것도 나쁘지 않음
 		
+//		System.out.println("=================================================");
+//		System.out.println("error type : " + e.getClass().getSimpleName());
+//		if(e instanceof NoHandlerFoundException) 
+//			System.out.println("true");
+//		System.out.println("=================================================");
+		
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		// 추후 로깅을 위함
@@ -33,6 +40,11 @@ public class GlobalExceptionHandler {
 		// 2. 사과 페이지 (HTML 응답, 정상 종료) 
 		return "error/exception";
 	}
+	
+//	@ExceptionHandler(NoHandlerFoundException.class)
+//	   public String Exception404() {
+//	      return "error/404";
+//	   }
 }
 
 
